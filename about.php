d1016b49054b2d5a47edce317fb42fde330650dd
Cristina M <mcristinamotoc@gmail.com>
2021-05-20 10:19:34 +0200

    add about info page

---
+++ about.php
@@ -0,0 +1,70 @@
+<?php include("connection.php"); ?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <script src="https://use.fontawesome.com/6234b7c045.js"></script>
+    <link href="css/style.css" rel="stylesheet">
+    <title> Reposteria Online Creativa</title>
+</head>
+
+<!-- Button Back to Main Page -->
+<form role="form" method="post" action="./index.php">
+    <input name="action" type="hidden" value="New">
+    <button name="id" type="submit" class="btn btn-info mx-5 my-3">Volver</button>
+</form>
+
+<!-- About Us information  -->
+<div class="card my-2">
+    <div class="card-body mx-3 my-1">
+        <h2 class="card-title display-5 text-center text-info">¡Diseñamos la tarta de tus sueños!</h2>
+        <p class="card-text">
+
+            ¡Hace casi 8 años comenzó esta aventura!
+            Empezó como un sueño, una oportunidad de cambiar, de emprender y de darle un giro a nuestras vidas...
+            Hemos vivido todo tipo de sensaciones y situaciones; tropiezos, triunfos y sobretodo aprendizajes.
+            Han habido momentos difíciles en los que sólo quieres tirar la toalla y dejarlo todo... pero han sido muchos
+            más los momentos de felicidad y satisfacción;
+            esos en los que sientes que todo vale la pena, que estás cumpliendo tu SUEÑO!
+
+        </p>
+        <p class="card-text">Gracias a todas las personas maravillosas que han formado parte de esta historia hasta el
+            día de hoy. Gracias a nuestras familias que han sido el motor principal.
+            A nuestros amigos y conocidos, los que están y los que ya no.... por su paciencia y cariño.
+            A todos los clientes que han confiado en nosotros en alguna ocasión, a los que con respeto y cariño nos han
+            manifestado sus opiniones y sus críticas.
+            A los que nos han herido y criticado de una forma dura, también gracias porque nos han motivado a aprender
+            de cada uno de nuestros errores y nos han "obligado" a esforzarnos y exigirnos más aún para ser mejores cada
+            día en todos los aspectos.
+            Mil y mil gracias a los que hoy siguen con nosotros, confiando en nuestro trabajo y haciéndonos parte de sus
+            momentos más especiales.</p>
+        <p class="card-text"> Hoy quiero presentarles nuestra nueva imagen, la que nos representa más que nunca y nos
+            seguirá representando de ahora en adelante en todos los increíbles proyectos que vienen MUY PRONTO...
+            Estamos muy cerca de poderles dar una super noticia que nos tiene más que felices!!! Ya casi .......😍😍😘🤗
+        </p>
+        <a href="contact.php" class="btn btn-info">Contacto</a>
+    </div>
+</div>
+</div>
+<!-- Including the Footer-->
+<?php include("footer.php"); ?>
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS and Font Awesome -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js "
+    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n " crossorigin="anonymous ">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js "
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo " crossorigin="anonymous ">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js "
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6 " crossorigin="anonymous ">
+</script>
+<script src="https://use.fontawesome.com/6234b7c045.js"></script>
